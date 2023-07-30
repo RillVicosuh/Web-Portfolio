3724c3dea23e7e23e88598276014a30b6d35df19
RillVicosuh <rickvill00@gmail.com>
2023-07-30 00:37:07 -0700

    trying new layout for footer again

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ const Footer = () => {
       className="w-full border-t-2 border-solid border-dark
     font-medium text-lg dark:text-light dark:border-light sm:text-base"
     >
-      <Layout className="py-8 flex flex-col sm:flex-row items-center justify-between lg:py-6">
+      <Layout className="py-8 flex flex-row sm:flex-col items-center justify-between lg:py-6 text-dark dark:text-light">
         <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
 
-        <div className="flex flex-col sm:flex-row items-center lg:py-2 text-dark dark:text-light">
+        <div className="flex flex-row sm:flex-col items-center lg:py-2 text-dark dark:text-light">
           Built with
           <span className="text-primary text-2xl px-1 dark:text-primaryDark">&#9825;</span>
           and thought by Ricardo Villacana
