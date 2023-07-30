f7d9c8f5ef74131744ad6367ee4920fcd09bf884
RillVicosuh <rickvill00@gmail.com>
2023-07-30 00:18:52 -0700

    Fixing the footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,26 +6,27 @@ const Footer = () => {
   return (
     <footer
       className="w-full border-t-2 border-solid border-dark
-    font-medium text-lg dark:text-light dark:border-light sm:text-base
-    "
+    font-medium text-lg dark:text-light dark:border-light sm:text-base"
     >
-      <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6">
+      <Layout className="py-8 flex flex-col sm:flex-row items-center justify-between lg:py-6">
         <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
 
-        <div className="flex items-center lg:py-2">
-          Built with <span className="text-primary text-2xl px-1 dark:text-primaryDark">&#9825;</span> and thought by Ricardo Villacana
+        <div className="flex flex-wrap items-center text-center lg:py-2 text-dark dark:text-light">
+          Built with
+          <span className="text-primary text-2xl px-1 dark:text-primaryDark">&#9825;</span> and
+          thought by Ricardo Villacana
         </div>
-
-        <Link
-          href="https://devdreaming.com/about"
-          target="_blank"
-          className="underline underline-offset-2"
-        >
-          Say Hello
-        </Link>
       </Layout>
     </footer>
   );
 };
 
 export default Footer;
+/*<Link
+          href="https://devdreaming.com/about"
+          target="_blank"
+          className="underline underline-offset-2"
+        >
+          Say Hello
+        </Link>
+*/
