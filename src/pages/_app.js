ff1bdc652022e2f895f3524126f8818eac2499fd
RillVicosuh <rickvill00@gmail.com>
2023-08-02 23:20:16 -0700

    Adding analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ import { AnimatePresence } from "framer-motion";
 import { Montserrat } from "next/font/google";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { Analytics } from '@vercel/analytics/react';
 
 // If loading a variable font, you don't need to specify the font weight
 const montserrat = Montserrat({ subsets: ["latin"], variable: "--font-mont" });
@@ -25,6 +26,7 @@ export default function App({ Component, pageProps }) {
         <Navbar />
         <AnimatePresence initial={false} mode="wait">
           <Component key={router.asPath} {...pageProps} />
+          <Analytics />
         </AnimatePresence>
         <Footer />
       </main>
